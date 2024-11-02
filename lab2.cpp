a92f7faedd2990530a21b9ba3cdbd78d2e579495
w1ndfallen <dlyashkolu192@gmail.com>
2024-11-02 23:24:51 +0200

    lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
-
-double distance(double x1, double y1, double x2, double y2) {
-    return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-}
 
 int main() {
     double x1, y1, x2, y2, x3, y3;
-
     cout << "Введіть координати першої вершини (x1 y1): ";
     cin >> x1 >> y1;
-
     cout << "Введіть координати другої вершини (x2 y2): ";
     cin >> x2 >> y2;
-
     cout << "Введіть координати третьої вершини (x3 y3): ";
     cin >> x3 >> y3;
 
-    double side1 = distance(x1, y1, x2, y2);
-    double side2 = distance(x2, y2, x3, y3);
-    double side3 = distance(x3, y3, x1, y1);
+    double side1 = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    double side2 = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
+    double side3 = sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2));
 
     double perimeter = side1 + side2 + side3;
 
