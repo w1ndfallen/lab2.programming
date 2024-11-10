19d07cb40063b14ae3e3bfe9b577a0e6b38c70b2
w1ndfallen <dlyashkolu192@gmail.com>
2024-11-10 13:06:52 +0200

    lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
 int main() {
     double x1, y1, x2, y2, x3, y3;
-    cout << "Введіть координати першої вершини (x1 y1): ";
+
+    cout << "Enter the coordinates of the first vertex (x1 y1): ";
     cin >> x1 >> y1;
-    cout << "Введіть координати другої вершини (x2 y2): ";
+
+    cout << "Enter the coordinates of the second vertex (x2 y2): ";
     cin >> x2 >> y2;
-    cout << "Введіть координати третьої вершини (x3 y3): ";
+
+    cout << "Enter the coordinates of the third vertex (x3 y3): ";
     cin >> x3 >> y3;
 
-    double side1 = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-    double side2 = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
-    double side3 = sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2));
+    double side1 = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    double side2 = sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+    double side3 = sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
 
     double perimeter = side1 + side2 + side3;
 
-    cout << "Периметр трикутника: " << perimeter << endl;
+    cout << "Perimeter of triangle: " << perimeter << endl;
 
     return 0;
 }
